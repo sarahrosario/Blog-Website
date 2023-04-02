8cb4c96cbe89c02f056c78be9dad6ad41d66647d
SARAH ZHANG <ciao.sarah@yahoo.com>
2023-04-01 20:47:55 -0400

    update secrect key

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ from functools import wraps
 import os
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = os.environ['MY_SECRECT_KEY']
+app.config['SECRET_KEY'] =os.environ.get("MY_SECRECT_KEY")
 ckeditor = CKEditor(app)
 Bootstrap(app)
 
